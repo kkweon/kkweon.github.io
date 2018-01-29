b9a2a994e92b25a26c37e3e21a2a49e483dc1750
Kyung Mo Kweon <kkweon@gmail.com>
2018-01-29 11:45:44 -0800

    [refact] change style & remove unnecessary code
    - refactor unnecessary codes
    - `code` tag style
    - after `.gatsby-highlight` code snippet, add some margin

--- src/components/AddThis/index.jsx
+++ src/components/AddThis/index.jsx
@@ -1,11 +1,6 @@
 import React, { Component } from "react";
 
 class AddThis extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   componentWillMount() {
     // Check if there is `document`
 
