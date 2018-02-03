192aff2a914d07188e92c9b5c3ee0ab82c78e431
Kyung Mo Kweon <kkweon@gmail.com>
2018-02-03 11:02:12 -0800

    [add] react-adsense
    1. Eject `index.html.js`

---
+++ src/html.js
@@ -0,0 +1,53 @@
+import React from "react";
+import AdSense from "react-adsense";
+
+let stylesStr;
+if (process.env.NODE_ENV === `production`) {
+  try {
+    stylesStr = require(`!raw-loader!../public/styles.css`);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+module.exports = class HTML extends React.Component {
+  render() {
+    let css;
+    if (process.env.NODE_ENV === `production`) {
+      css = (
+        <style
+          id="gatsby-inlined-css"
+          dangerouslySetInnerHTML={{ __html: stylesStr }}
+        />
+      );
+    }
+    return (
+      <html {...this.props.htmlAttributes}>
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <script
+            async
+            src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          />
+          {this.props.headComponents}
+          {css}
+        </head>
+        <body {...this.props.bodyAttributes}>
+          {this.props.preBodyComponents}
+          <div
+            key={`body`}
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+          <AdSense.Google client="ca-pub-2420244930131504" slot="" />
+        </body>
+      </html>
+    );
+  }
+};
