041d84738d499500fc7323303a3b070c81c2cf98
Mo Kweon <kkweon@google.com>
2020-07-17 10:29:26 -0700

    fix: add favicon

--- src/html.tsx
+++ src/html.tsx
@@ -30,6 +30,24 @@ export default class HTML extends React.Component<IHtmlProps, void> {
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="shortcut icon" href="/favicon.ico" />
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
