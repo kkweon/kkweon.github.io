a995295c0176b7b6d5ee1ccbfe6f38d48181a07c
Mo Kweon <kkweon@google.com>
2020-08-13 00:55:11 -0700

    Replace gatsby-plugin-google-analytics with gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,11 +21,27 @@ module.exports = {
       resolve: `gatsby-plugin-sitemap`,
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: 'UA-69116729-1',
-        // Setting this parameter is optional
-        anonymize: false,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'UA-69116729-1', // Google Analytics
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: false,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ['/preview/**', '/do-not-track/me/too/'],
+        },
       },
     },
     {
