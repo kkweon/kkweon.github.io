77467090e9895a316bad52d87f0feae7e51d91b0
Mo Kweon <kkweon@google.com>
2020-09-07 11:51:51 -0700

    Revert "Add gatsby-route-change custom event"
    This reverts commit 45748143671a8ee811abbb1067e7cd84297e6761.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,12 +3,5 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
-exports.onRouteUpdate = () => {
-  // wrap inside a timeout to ensure the title has properly been changed
-  setTimeout(() => {
-    let data = window.dataLayer
-    let eventName = `gatsby-route-change`
 
-    data.push({ event: eventName })
-  }, 50)
-}
+// You can delete this file if you're not using it
