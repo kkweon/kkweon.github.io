f2e23137940d34920dfaff7bb706ae913abb0c9c
Mo Kweon <kkweon@google.com>
2021-09-20 17:25:03 -0700

    fix: new_post.sh taks a title now

--- new_post.sh
+++ new_post.sh
@@ -4,14 +4,19 @@ TODAY=$(date '+%Y-%m-%d')
 NOW=$(date '+%Y-%m-%dT%T%z')
 BASE_DIR="src/posts"
 
-TITLE="$1"
+# TITLE="Hello world"
+TITLE="${1^}"
+# TITLE_TEMP="Hello-world"
+TITLE_TEMP=${TITLE// /-}
+# TITLE_HYPHEN="hello-world"
+TITLE_HYPHEN=${TITLE_TEMP,,}
 
-if [ -z "$TITLE" ]; then
-  echo "$TITLE is not given"
+if [ -z "$TITLE_HYPHEN" ]; then
+  echo "$TITLE_HYPHEN is not given"
   exit 1
 fi
 
-FINAL_PATH_DIR="$BASE_DIR/$TODAY-$TITLE"
+FINAL_PATH_DIR="$BASE_DIR/$TODAY-$TITLE_HYPHEN"
 mkdir "$FINAL_PATH_DIR"
 cat << EOF > "$FINAL_PATH_DIR/index.md"
 ---
